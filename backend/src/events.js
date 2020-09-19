ec9d2774f750929920716df398142cccd8863044
randolph <randolph.estuardo.2012@gmail.com>
2020-09-19 17:13:23 -0600

    Actualizacion de un producto
    se actualizo, un registro de la tabla producto por medio de su Id

--- backend/src/events.js
+++ backend/src/events.js
@@ -11,6 +11,25 @@ function createRouter(db) {
 *****************************************************************
 */
 
+//******Acutalizar producto******
+router.put('/actualizar_producto', (req, res, next) => {
+    db.query(
+      'UPDATE PRODUCTO SET nombre=?,precio=?,descripcion=? WHERE id_producto=?',
+      [req.body.nombre,req.body.precio,req.body.descripcion,req.body.id_producto],
+      (error) => {
+        if(error)
+        {
+          console.error(error);
+          res.status(500).json({status:'error'});
+        }
+        else
+        {
+          res.status(200).json({status:'ok'});
+        }
+      }
+    );
+});
+
 //******eliminar producto******
 router.put('/eliminar_producto', (req, res, next) => {
 
@@ -33,7 +52,7 @@ router.put('/eliminar_producto', (req, res, next) => {
 });
 
 
-router.post('/producto/nuevo', (req, res, next) => {
+router.post('/nuevo_producto', (req, res, next) => {
   db.query(
     'INSERT INTO PRODUCTO(nombre,precio,descripcion) VALUES(?,?,?)',
     [req.body.nombre,req.body.precio,req.body.descripcion],
