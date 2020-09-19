c5dbf8b7ab20299c23cf699b440266bdbd3129ca
randolph <randolph.estuardo.2012@gmail.com>
2020-09-19 16:36:30 -0600

    creacion de registro de nuevo producto

--- backend/src/events.js
+++ backend/src/events.js
@@ -5,10 +5,37 @@ function createRouter(db) {
   const owner = '';
 
   // the routes are defined here
-
+/*
+*****************************************************************
+************Crud de Productos************
+*****************************************************************
+*/
+router.post('/producto/nuevo', (req, res, next) => {
+  db.query(
+    'INSERT INTO PRODUCTO(nombre,precio,descripcion) VALUES(?,?,?)',
+    [req.body.nombre,req.body.precio,req.body.descripcion],
+    (error) => {
+      if(error)
+      {
+        console.error(error);
+        res.status(500).json({status:'error'});
+      }
+      else
+      {
+        res.status(200).json({status:'ok'});
+      }
+    }
+  )
+});
 
   return router;
 }
+
+
+
+
+
+
 module.exports = createRouter;
 
 
