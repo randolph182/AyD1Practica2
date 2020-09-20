2da84f7d86756dd45e3694b03a9cfe09efa2b096
randolph <randolph.estuardo.2012@gmail.com>
2020-09-19 20:15:35 -0600

    Prueba unitaria para Insertar Productos
    Se implementa la primera estructura para obtener satisfactorio en la prueba de hacer una inserción de un producto.

--- backend/src/events.js
+++ backend/src/events.js
@@ -271,6 +271,24 @@ router.post('/nuevo_producto', (req, res, next) => {
 }
 
 
+//metodo que obtiene el ultimo registro ingresado de los productos
+  router.get('/ultimo_producto', (req, res, next) => {
+    db.query(
+      'SELECT MAX(id_producto) AS id FROM USUARIO',
+      (error, results) => {
+        if(error)
+        {
+          console.error(error);
+          res.status(500).json({status:'error'});
+        }
+        else
+        {
+          res.status(200).json(results);
+        }
+      }
+    );
+  });
+  
 
 
 
--- backend/test/test.js
+++ backend/test/test.js
@@ -4,12 +4,29 @@ const expect = require('chai').expect;
 chai.use(chaiHttp);
 const url = 'http://localhost:3000';
 
-   //Informacion de productos
-   describe.only('Prueba para obtener informacion de la API: ',()=>{
-      it('Debe poder obtener una lista de productos', async ()=>{
-          let res = await chai
-          .request(url)
-          .get('/productos'); //obteniendo estudiantes
-          expect(res.status).to.equal(200);
-      });
-  });
+//Informacion de productos
+describe('Prueba para obtener informacion de la API: ',()=>{
+   it('Debe poder obtener una lista de productos', async ()=>{
+         let res = await chai
+         .request(url)
+         .get('/productos'); //obteniendo estudiantes
+         expect(res.status).to.equal(200);
+   });
+});
+
+describe.only('Prueba para insertar un nuevo producto a la API: ',()=>{
+   it('Prueba para Insertar un nuevo producto', (done)=>{
+      chai.request(url)
+            .post('/nuevo_producto')
+            .send({nombre: "consome maller",precio:23,descripcion:"El del pollito"})
+            .end(function(err,res){
+               console.log(res.body)
+               expect(res).to.have.status(500);
+               done();
+            });
+   });
+});
+
+//insercion  de productos
+
+
