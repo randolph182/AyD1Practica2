5892b10bfecbcfa158d3a603bc4f78c3bcaa4db9
Alan Jeremias Perez <aaperez.tomas@gmail.com>
2020-09-19 19:52:06 -0600

    Inicio pruebas unitarias

--- frontend/src/app/services/usuario.service.spec.ts
+++ frontend/src/app/services/usuario.service.spec.ts
@@ -1,13 +1,39 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject, async} from '@angular/core/testing';
 
 import { UsuarioService } from './usuario.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import { Usuario } from '../models/usuario';
 
 describe('UsuarioService', () => {
   let service: UsuarioService;
+  const mockResponse:Usuario = new Usuario();
+
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers:[UsuarioService]
+    });
     service = TestBed.inject(UsuarioService);
+  });
+
+
+  describe('get data', () => {
+    it('should get results',
+    inject([HttpTestingController, UsuarioService], (httpMock: HttpTestingController, usuarioServiceTested: UsuarioService) => {
+      const swapiUrl = 'https://localhost:3000/obtener_usuario';
+      usuarioServiceTested.getUsuarios()
+      .subscribe(
+        (res) => {
+          expect(res).toEqual();
+        }
+      );
+      const req = httpMock.expectOne(swapiUrl);
+      expect(req.request.method).toBe('GET');
+      req.flush(mockResponse);
+    })
+  );
   });
 
   it('should be created', () => {
