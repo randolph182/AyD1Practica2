582cbc3eaa10e2d1bc87d89d6b1caf5b137f11dc
randolph <randolph.estuardo.2012@gmail.com>
2020-09-19 19:41:39 -0600

    Prueba unitaria sobre peticion de productos
    Se Realizo la prueba unitaria, sobre la obtención de productos de la base de datos

--- backend/test/test.js
+++ backend/test/test.js
@@ -4,3 +4,12 @@ const expect = require('chai').expect;
 chai.use(chaiHttp);
 const url = 'http://localhost:3000';
 
+   //Informacion de productos
+   describe.only('Prueba para obtener informacion de la API: ',()=>{
+      it('Debe poder obtener una lista de productos', async ()=>{
+          let res = await chai
+          .request(url)
+          .get('/productos'); //obteniendo estudiantes
+          expect(res.status).to.equal(200);
+      });
+  });
